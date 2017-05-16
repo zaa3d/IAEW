3bff7b2abecffe644e12c8426d87f60a1c4591f0
zaa3d <zidtamim@outlook.com>
2017-05-16 21:46:03 +0200

    TrassenParameter wurde aufgefüllt

--- LadeNetzdaten.m
+++ LadeNetzdaten.m
@@ -1,50 +1,86 @@
 %% Aufgabe 1.1 - Modellierung des Netzes
                           
 %TrassenParameter 
-%                   Anfangs-      Endknoten    Widerstand      Reaktanz    Maximale       Investitions-      Anzahl der         Maximale Anzahl der
-%                    knoten                     						   Belastung          kosten		 Leitungen		       Leitungen
+%                   Anfangs- Endknoten Widerstand     Reaktanz /   Maximale   / Invest-     / Anzahl der / Maximale Anzahl der
+%                    knoten                                       Belastung    itionskosten    Leitungen     Leitungen
 
-TrassenParameter = [   1              2            0.003         0.014        175                3               1                    3;
-                       1              3            0.055         0.211        175                55              1                    3;
-                       1              4            0.022         0.085        175                22              1                    3;
-                       2              4            0.033         0.127        175                33              1                    3
-                    %ToDo: Fuelle die Matrix "TrassenParameter" mit den zugehˆrigen Werten
+TrassenParameter = [   1        2       0.003    	0.014   	175 	3       1 	3;
+                       1        3       0.055    	0.211   	175   	55      1       3;
+                       1        5       0.022    	0.085   	175     22      1       3;
+                       2        4       0.033    	0.127   	175     33      1       3;
+		       2 	6 	0.05	 	0.192	 	175  	50	1	3;
+		       3 	9 	0.031	 	0.119		175 	31 	1 	3;
+		       3 	24 	0.002 		0.084 		400 	50 	1 	3;
+		       4 	9 	0.027 		0.104 		175 	27 	1 	3;
+		       5 	10 	0.023 		0.088 		175 	23 	1 	3;
+		       6 	10 	0.014 		0.061 		175 	16 	1	3;
+		       7 	8 	0.016 		0.061		175 	16 	1 	3;
+		       8 	9 	0.043 		0.165 		175 	43 	1 	3;
+		       8 	10 	0.043 		0.165 		175 	43 	1 	3;
+		       9 	11 	0.002 		0.084 		400 	50 	1 	3;
+		       9 	12 	0.002 		0.084 		400 	50 	1 	3;
+		       10 	11 	0.002 		0.084 		400 	50 	1 	3;
+		       10 	12 	0.002 		0.084 		400 	50 	1 	3;
+		       11 	13 	0.006 		0.048 		500 	33 	1 	3;
+		       11 	14 	0.005 		0.042 		500 	29 	1 	3;
+		       12 	13 	0.006 		0.048 		500 	33	1 	3;
+		       12 	23 	0.012 		0.097 		500 	67 	1 	3;
+		       13 	23 	0.011		0.087 		500 	60 	1 	3;
+		       14 	16 	0.005 		0.059 		500 	27 	1 	3;
+		       15 	16 	0.002 		0.017 		500 	12 	1 	3;
+		       15 	21 	0.006 		0.049 		500 	34 	2 	3;
+		       15 	24 	0.007 		0.052 		500 	36 	1 	3;
+		       16 	17 	0.003 		0.026 		500 	18 	1 	3;
+		       16 	19 	0.003 		0.023 		500	16 	1 	3;
+		       17 	18 	0.002 		0.014 		500 	10	1 	3;
+		       17 	22 	0.014 		0.105 		500 	73 	1 	3;
+		       18 	21 	0.003 		0.026 		500 	18 	2 	3;
+		       19 	20 	0.005 		0.04 		500 	27.5 	2 	3;
+		       20 	23 	0.003 		0.022 		500 	15 	2 	3;
+		       21 	22 	0.009 		0.068	 	500 	47 	1 	3;
+		       1 	8 	0.035 		0.134 		175 	35 	0 	3;
+		       2 	8 	0.033 		0.127 		175 	33 	0 	3;
+		       6 	7 	0.05 		0.192 		175 	50 	0 	3;
+		       13 	14 	0.006 		0.045 		500 	62 	0 	3;
+		       14 	23 	0.008 		0.062 		500 	86 	0 	3;
+		       16 	23 	0.011 		0.082 		500 	114 	0 	3;
+		       19 	23 	0.008 		0.061 		500 	84 	0 	3;%ToDo: Fuelle die Matrix "TrassenParameter" mit den zugeh√∂rigen Werten
                    ];
        
 
-%ToDo: Lege einzelne Vektoren f¸r jede einzelne Spalte der Matrix TrassenParameter an, indem du die Spalten aus der Matrix extrahierst
-Anfangsknoten =    TrassenParameter(:,1); %Vektor, der den Index des Anfangsknotens der Trassen enth‰lt
-Endknoten =  TrassenParameter(:,2);%Vektor, der den Index des Endknotens der Trassen enth‰lt
-Widerstand = TrassenParameter(:,3);    %Vektor, der die Widerst‰nde einer Leitung der Trassen enth‰lt
-Reaktanz =  TrassenParameter(:,4);   %Vektor, der die Reaktanzen einer Leitung der Trassen enth‰lt
-Maximale_Belastung = TrassenParameter(:,5);    %Vektor, der die maximal zul‰ssige Belastungen einer Leitung der Trassen enth‰lt
-Investitionskosten = TrassenParameter(:,6);    %Vektor, der die Investitionskosten einer Leitung der Trassen enth‰lt 
-Anzahl_Stromkreise = TrassenParameter(:,7);    %Vektor, der die Anzahl der existierenden Stromkreise auf den Trassen enth‰lt 
-Maximale_Anzahl_Stromkreise = TrassenParameter(:,8);    %Vektor, der die Anzahl mˆglicher Stromkreise auf den Trassen enth‰lt 
+%ToDo: Lege einzelne Vektoren f√ºr jede einzelne Spalte der Matrix TrassenParameter an, indem du die Spalten aus der Matrix extrahierst
+Anfangsknoten =    TrassenParameter(:,1); %Vektor, der den Index des Anfangsknotens der Trassen enth√§lt
+Endknoten =  TrassenParameter(:,2);%Vektor, der den Index des Endknotens der Trassen enth√§lt
+Widerstand = TrassenParameter(:,3);    %Vektor, der die Widerst√§nde einer Leitung der Trassen enth√§lt
+Reaktanz =  TrassenParameter(:,4);   %Vektor, der die Reaktanzen einer Leitung der Trassen enth√§lt
+Maximale_Belastung = TrassenParameter(:,5);    %Vektor, der die maximal zul√§ssige Belastungen einer Leitung der Trassen enth√§lt
+Investitionskosten = TrassenParameter(:,6);    %Vektor, der die Investitionskosten einer Leitung der Trassen enth√§lt 
+Anzahl_Stromkreise = TrassenParameter(:,7);    %Vektor, der die Anzahl der existierenden Stromkreise auf den Trassen enth√§lt 
+Maximale_Anzahl_Stromkreise = TrassenParameter(:,8);    %Vektor, der die Anzahl m√∂glicher Stromkreise auf den Trassen enth√§lt 
 
 
 
 %% Aufgabe 1.2 - Modellierung des Last-/Einspeisefalles
 
-%Tipp: Kopiere und benenne das MatLab-Skript entsprechend, um die einzelnen Netznutzungsf‰lle zu parametrieren 
+%Tipp: Kopiere und benenne das MatLab-Skript entsprechend, um die einzelnen Netznutzungsf√§lle zu parametrieren 
 
 %KnotenParameter
 %                  Knoten-       Knoten-           Spannung               Last               Erzeugung    
 %                  nummer         typ          Betrag    Winkel        MW     Mvar         MW      Mvar 
 
 KnotenParameter = [    
-                   %ToDo: Fuelle die Matrix "KnotenParameter" mit den zugehˆrigen Werten
+                   %ToDo: Fuelle die Matrix "KnotenParameter" mit den zugeh√∂rigen Werten
 				  ];
   
 
-%ToDo: Lege einzelne Vektoren f¸r jede einzelne Spalte der Matrix KnotenParameter an, indem du die Spalten aus der Matrix extrahierst
-Knotenummer = KnotenParameter(:,1);    %Vektor, der die Knotennummer der Knoten enth‰lt
-Knotentyp = KnotenParameter(:,2);    %Vektor, der den Knotentypen der Knoten enth‰lt
-Knotenspannung_Betrag = KnotenParameter(:,3);    %Vektor, der den Betrag der Knotenspannung der Knoten enth‰lt (Tipp: Startwert 1.0)
-Knotenspannung_Winkel = KnotenParameter(:,4);    %Vektor, der den Winkel der Knotenspannung der Knoten enth‰lt (Tipp: Startwert 0.0)
-Last_MW = KnotenParameter(:,5);    %Vektor, der die Wirkleistungslast anliegend an den Knoten enth‰lt
-Last_Mvar = KnotenParameter(:,6);    %Vektor, der die Blindleistungslast anleigend an den Knoten enth‰lt
-Erzeugung_MW = KnotenParameter(:,7);    %Vektor, der die Wirkleistungserzeugung anliegend an den Knoten enth‰lt
-Erzeugung_Mvar = KnotenParameter(:,8);    %Vektor, der die Blindleistungserzeugung anliegend an den Knoten enth‰lt
+%ToDo: Lege einzelne Vektoren f√ºr jede einzelne Spalte der Matrix KnotenParameter an, indem du die Spalten aus der Matrix extrahierst
+Knotenummer = KnotenParameter(:,1);    %Vektor, der die Knotennummer der Knoten enth√§lt
+Knotentyp = KnotenParameter(:,2);    %Vektor, der den Knotentypen der Knoten enth√§lt
+Knotenspannung_Betrag = KnotenParameter(:,3);    %Vektor, der den Betrag der Knotenspannung der Knoten enth√§lt (Tipp: Startwert 1.0)
+Knotenspannung_Winkel = KnotenParameter(:,4);    %Vektor, der den Winkel der Knotenspannung der Knoten enth√§lt (Tipp: Startwert 0.0)
+Last_MW = KnotenParameter(:,5);    %Vektor, der die Wirkleistungslast anliegend an den Knoten enth√§lt
+Last_Mvar = KnotenParameter(:,6);    %Vektor, der die Blindleistungslast anleigend an den Knoten enth√§lt
+Erzeugung_MW = KnotenParameter(:,7);    %Vektor, der die Wirkleistungserzeugung anliegend an den Knoten enth√§lt
+Erzeugung_Mvar = KnotenParameter(:,8);    %Vektor, der die Blindleistungserzeugung anliegend an den Knoten enth√§lt
 
 
