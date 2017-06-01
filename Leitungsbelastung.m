51030af0e736000f99e5c2cabdeda0d8bf40bf52
zaa3d <zidtamim@outlook.com>
2017-06-01 12:21:21 +0200

    01.06

--- Leitungsbelastung.m
+++ Leitungsbelastung.m
@@ -1,8 +1,8 @@
 %% Aufgabe 1.5 - Bestimmung der Leitungsbelastung
 
 %%Vorgegebene Definitionen und Initialisierungen
-UeberlasteteTrassen = 0;    % Anzahl der überlasteten Trassen
-UeberlastungGesamt = 0;    % Summe der Leistungen über der maximalen Belastung
+UeberlasteteTrassen = 0;    % Anzahl der Ã¼berlasteten Trassen
+UeberlastungGesamt = 0;    % Summe der Leistungen Ã¼ber der maximalen Belastung
 
 
 %ToDo: Bestimme die Leitungsbelastung sowie die Verluste der Leitungen aus den berechneten Spannungswerten der Knoten
@@ -13,12 +13,12 @@ for Trasse = 1:AnzahlTrassen;
     Knoten_end = Endknoten(Trasse);
     
     %ToDo: Bestimme den Strom am Anfang und am Ende der Trasse (Tipp: Definition der Stromrichtung ist vom Knoten weg) [p.u.]
-    I_anf = %...;
-    I_end = %...;
+    I_anf = AMatrix(Knoten_anf,:)*Spannung_komplex;
+    I_end = AMatrix(Knoten_end,:)*Spannung_komplex;
     
-    %ToDo: Bestimme die Scheinleistung am Anfang und am Ende der Trasse (Tipp: Benutze dafür den Strom am Anfang bzw. am Ende der Trasse) [MVA]   
-    Scheinleistung_anf = %...;
-    Scheinleistung_end = %...;
+    %ToDo: Bestimme die Scheinleistung am Anfang und am Ende der Trasse (Tipp: Benutze dafÃ¼r den Strom am Anfang bzw. am Ende der Trasse) [MVA]   
+    Scheinleistung_anf = Spannung_komplex(Knoten_anf)*conj(I_anf);
+    Scheinleistung_end = Spannung_komplex(Knoten_end)*conj(I_end);
     
     
     %ToDo: Bestimme die Trassenverluste
@@ -33,16 +33,16 @@ for Trasse = 1:AnzahlTrassen;
     BelastungLeitungen(Trasse) = %...;
     
     
-    %ToDo: Bestimme die Auslastung(maximaler Leistungsfluss bezogen auf die maximal zulässige Belastung) der einzelnen Leitungen [%]
+    %ToDo: Bestimme die Auslastung(maximaler Leistungsfluss bezogen auf die maximal zulÃ¤ssige Belastung) der einzelnen Leitungen [%]
     AuslastungLeitungen(Trasse) = %...;
     
     
     if(AuslastungLeitungen(Trasse) > 100)
        
-        %ToDo: Bestimme wie viele Trassen überlastet sind [Anzahl]
+        %ToDo: Bestimme wie viele Trassen Ã¼berlastet sind [Anzahl]
         UeberlasteteTrassen = %...;
         
-        %ToDo: Bestimme um welche Leistung die maximale Belastbarkeit überschritten wird [MVA]
+        %ToDo: Bestimme um welche Leistung die maximale Belastbarkeit Ã¼berschritten wird [MVA]
         UeberlastungGesamt = %...;
 		
     end
